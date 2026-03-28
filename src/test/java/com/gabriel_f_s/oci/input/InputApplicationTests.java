b890f30a813fae8cad5b4602aee191e154b4be73
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-03-28 12:27:25 -0300

    adding initial files

---
+++ src/test/java/com/gabriel_f_s/oci/input/InputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_f_s.oci.input;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
