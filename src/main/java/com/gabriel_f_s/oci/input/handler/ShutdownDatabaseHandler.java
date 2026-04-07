265f530225a1c4f2066c23b7decdcaf132ba146a
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-04-07 15:46:35 -0300

    feat: a handler was created to change the log status from in_progress to canceled if the application stops unexpectedly.

---
+++ src/main/java/com/gabriel_f_s/oci/input/handler/ShutdownDatabaseHandler.java
@@ -0,0 +1,55 @@
+package com.gabriel_f_s.oci.input.handler;
+
+import com.gabriel_f_s.oci.input.entity.enums.ProgressStatus;
+import com.gabriel_f_s.oci.input.repository.AuditLogRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.SmartLifecycle;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ShutdownDatabaseHandler implements SmartLifecycle {
+
+    private static final Logger logger = LoggerFactory.getLogger(ShutdownDatabaseHandler.class);
+    private boolean isRunning = false;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ShutdownDatabaseHandler(AuditLogRepository repository, JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public void stop() {
+        logger.info("Performing status cleanup before full shutdown...");
+        try {
+            String sql = "UPDATE audit_logs SET status = ?, end_date = ?, error = ? WHERE status = ?";
+            int rows = jdbcTemplate.update(sql,
+                    ProgressStatus.CANCELED.name(),
+                    LocalDateTime.now(),
+                    "Application closed unexpectedly.",
+                    ProgressStatus.IN_PROGRESS.name()
+                    );
+            logger.info("Status updated successfully! Rows affected: {}", rows);
+        } catch (Exception e) {
+            logger.error("Error updating status: " + e.getMessage());
+        }
+        this.isRunning = false;
+    }
+
+    @Override
+    public void start() { this.isRunning = true; }
+
+    @Override
+    public boolean isRunning() { return this.isRunning; }
+
+    @Override
+    public int getPhase() {
+        return Integer.MIN_VALUE;
+    }
+}
