c96df83ef5d91c2960be8ab32f3d5860a883afaf
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-04-09 20:28:45 -0300

    feat: new logging methods

--- src/main/java/com/gabriel_f_s/oci/input/service/LoggingService.java
+++ src/main/java/com/gabriel_f_s/oci/input/service/LoggingService.java
@@ -4,6 +4,7 @@ import com.gabriel_f_s.oci.input.entity.AuditLog;
 import com.gabriel_f_s.oci.input.entity.enums.ProgressStatus;
 import com.gabriel_f_s.oci.input.exception.EntityNotFoundException;
 import com.gabriel_f_s.oci.input.repository.AuditLogRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,10 +17,12 @@ public class LoggingService {
 
     private final WebScrapingService webScrapingService;
     private final AuditLogRepository logRepository;
+    private final JdbcTemplate jdbcTemplate;
 
-    public LoggingService(WebScrapingService webScrapingService, AuditLogRepository logRepository) {
+    public LoggingService(WebScrapingService webScrapingService, AuditLogRepository logRepository, JdbcTemplate jdbcTemplate) {
         this.logRepository = logRepository;
         this.webScrapingService = webScrapingService;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     /**
@@ -39,11 +42,16 @@ public class LoggingService {
             AuditLog auditLog = optionalAuditLog
                     .orElseThrow(() -> new EntityNotFoundException("No log could be found"));
             LocalDateTime logEndDate = auditLog.getEndDate();
+            if (logEndDate == null) return false;
+
             return logEndDate.isBefore(LocalDateTime.now());
         }
         return false;
     }
 
+    /**
+     * Check if there are any logs with an in_progress status within the last 24 hours.
+     */
     public boolean hasLogInProgressUnderADay() {
         Optional<AuditLog> optionalAuditLog = logRepository.findFirstByStatusOrderByStartDate(ProgressStatus.IN_PROGRESS);
         return optionalAuditLog.isPresent();
@@ -57,8 +65,7 @@ public class LoggingService {
      *      Files to compare and verify if they match the log.
      */
     public List<String> checkProcessedFiles(AuditLog log, List<String> files) {
-        AuditLog auditLog = findAuditLogById(log);
-        List<String> processedFilesInLog = auditLog.getFilesProcessed();
+        List<String> processedFilesInLog = log.getFilesProcessed();
 
         if (processedFilesInLog.isEmpty()) return files;
 
@@ -76,10 +83,43 @@ public class LoggingService {
                     .orElseThrow(() -> new EntityNotFoundException("No log could be found"));
             auditLog.setStatus(ProgressStatus.IN_PROGRESS);
             auditLog.setStartDate(LocalDateTime.now());
+            auditLog.setLastHeartbeat(LocalDateTime.now());
             auditLog.setEndDate(null);
             return auditLog;
         }
         return null;
+    }
+
+    /**
+     * Updates last current file.
+     * @param log
+     *       Log to update.
+     * @param file
+     *      File name to insert in log (filename.zip)
+     */
+    public void updateCurrentFile(AuditLog log, String file) {
+        log.setCurrentFileName(file);
+        log.setLastHeartbeat(LocalDateTime.now());
+        logRepository.save(log);
+    }
+
+    /**
+     * Updates last current file.
+     * @param logId
+     *       Id of a log to update.
+     * @param record
+     *      File name to insert in log (filename.zip)
+     */
+    public void updateRecordsInserted(Long logId, int record) {
+        String selectSql = String.format("SELECT records_inserted FROM audit_logs WHERE id = %d", logId);
+        Long actualRecord = jdbcTemplate.queryForObject(
+                    selectSql,
+                    Long.class
+                );
+        actualRecord += record;
+
+        String sql = "UPDATE audit_logs SET records_inserted = ?, last_heartbeat = NOW() WHERE id = ?";
+        jdbcTemplate.update(sql, actualRecord, logId);
     }
 
     /**
@@ -90,10 +130,12 @@ public class LoggingService {
      *      File name to insert in log (filename.zip)
      */
     public void updateProcessedFiles(AuditLog log, String file) {
-        AuditLog auditLog = findAuditLogById(log);
-        if (!auditLog.getFilesProcessed().contains(file))
-            auditLog.addFileProcessed(file);
-        logRepository.save(auditLog);
+        if (!log.getFilesProcessed().contains(file)) {
+            log.addFileProcessed(file);
+            log.setRecordInserted(0L);
+        }
+        log.setLastHeartbeat(LocalDateTime.now());
+        logRepository.save(log);
     }
 
     /**
@@ -104,7 +146,26 @@ public class LoggingService {
     public void updateCanceledLog(AuditLog log) {
         log.setStatus(ProgressStatus.IN_PROGRESS);
         log.setError(null);
+        log.setLastHeartbeat(LocalDateTime.now());
         logRepository.save(log);
+    }
+
+    /**
+     * Updates a in_progress log to error, in case of error.
+     * @param error
+     *      Log to update.
+     */
+    public void updateLogInCaseOfError(String error) {
+        Optional<AuditLog> optionalLog = logRepository.findFirstByStatusOrderByStartDate(ProgressStatus.IN_PROGRESS);
+        if (optionalLog.isEmpty()) return;
+
+        AuditLog log = optionalLog.get();
+        int timesStopped = log.getNumberOfTimesItStopped();
+        log.setStatus(ProgressStatus.ERROR);
+        log.setEndDate(LocalDateTime.now());
+        log.setLastHeartbeat(LocalDateTime.now());
+        log.setNumberOfTimesItStopped(timesStopped + 1);
+        log.setError(error);
     }
 
     /**
@@ -113,6 +174,7 @@ public class LoggingService {
     public AuditLog createNewLog() {
         AuditLog log = new AuditLog(
                 ProgressStatus.IN_PROGRESS,
+                LocalDateTime.now(),
                 LocalDateTime.now()
         );
         logRepository.save(log);
@@ -127,8 +189,9 @@ public class LoggingService {
     public void finishLog(AuditLog log) {
         log.setLastFolderProcessed(webScrapingService.getLastMonth());
         log.setStatus(ProgressStatus.FINISHED);
+        log.setLastHeartbeat(LocalDateTime.now());
         log.setEndDate(LocalDateTime.now());
-        log.setTimeSpentInHours(ChronoUnit.HOURS.between(log.getStartDate(), log.getEndDate()));
+        log.setTotalTimeSpentInMs(ChronoUnit.MILLIS.between(log.getStartDate(), log.getEndDate()));
         logRepository.save(log);
     }
 
