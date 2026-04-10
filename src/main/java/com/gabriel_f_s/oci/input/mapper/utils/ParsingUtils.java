add1f37cb1034e11634d272377d57b1cde0405a3
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-04-10 11:33:42 -0300

    feat: static methods to parsing objects

---
+++ src/main/java/com/gabriel_f_s/oci/input/mapper/utils/ParsingUtils.java
@@ -0,0 +1,26 @@
+package com.gabriel_f_s.oci.input.mapper.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class ParsingUtils {
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    public static LocalDate parseLocalDate(String dateStr) {
+        if (dateStr == null || dateStr.isBlank() || "00000000".equals(dateStr) || "0".equals(dateStr)) {
+            return null;
+        }
+        try {
+            return LocalDate.parse(dateStr.trim(), DATE_FORMATTER);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    public static Integer parseInteger(String intStr) {
+        if (intStr == null) return null;
+        return Integer.parseInt(intStr);
+    }
+}
