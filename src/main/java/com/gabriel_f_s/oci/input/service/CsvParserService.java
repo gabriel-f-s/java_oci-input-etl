b079e67cda197a2ab54e314cdba0499185eb14d0
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-04-12 21:28:22 -0300

    fix: adjusting the CSV file reading to avoid errors in case the CSV file has poor formatting

--- src/main/java/com/gabriel_f_s/oci/input/service/CsvParserService.java
+++ src/main/java/com/gabriel_f_s/oci/input/service/CsvParserService.java
@@ -1,9 +1,15 @@
 package com.gabriel_f_s.oci.input.service;
 
 import com.gabriel_f_s.oci.input.entity.AuditLog;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.enums.CSVReaderNullFieldIndicator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStreamReader;
@@ -16,6 +22,7 @@ import java.util.zip.ZipInputStream;
 @Service
 public class CsvParserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CsvParserService.class);
     private final int BATCH_SIZE = 5000;
     private final LoggingService loggingService;
 
@@ -33,27 +40,37 @@ public class CsvParserService {
 
         long checkpoint = log.getRecordInserted();
 
-        CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(input)
-                .withType(dtoType)
+        CSVParser parser = new CSVParserBuilder()
                 .withSeparator(';')
+                .withIgnoreQuotations(true)
+                .build();
+
+        CSVReader csvReader = new CSVReaderBuilder(input)
+                .withCSVParser(parser)
+                .withSkipLines(Math.toIntExact(checkpoint))
+                .build();
+
+        CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(csvReader)
+                .withType(dtoType)
                 .withFieldAsNull(CSVReaderNullFieldIndicator.BOTH)
                 .withIgnoreLeadingWhiteSpace(true)
                 .withIgnoreEmptyLine(true)
-                .withSkipLines(Math.toIntExact(checkpoint))
+                .withExceptionHandler(e -> {
+                    logger.warn("The line number {}, containing the content {}, could not be read. Error: {}", e.getLine(), e.getLineNumber(), e.getMessage());
+                    return null;
+                })
                 .build();
 
         List<T> batch = new ArrayList<>();
 
         for (T dto : csvToBean) {
             batch.add(dto);
-
             if (batch.size() >= BATCH_SIZE) {
                 databaseSaveAction.accept(batch);
                 loggingService.updateRecordsInserted(log.getId(), batch.size());
                 batch.clear();
             }
         }
-
         if (!batch.isEmpty()) {
             databaseSaveAction.accept(batch);
             loggingService.updateRecordsInserted(log.getId(), batch.size());
